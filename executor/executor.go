d7636d662b7eb83397686c55a63efe1b17ad2a91
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-03-04 16:43:28 +0000

    Add all versions

--- executor/executor.go
+++ executor/executor.go
@@ -1,7 +1,9 @@
 package executor
 
 import (
+	"fmt"
 	"github.com/varlink/go/varlink"
+	"os"
 )
 
 func Init(address string) (*varlink.Connection, error) {
@@ -14,6 +16,13 @@ func Init(address string) (*varlink.Connection, error) {
 }
 
 func GetPodmanVersion(conn *varlink.Connection) string {
-	version, goVersion, commitVersion, _, _, _, _ := GetVersion().Call(conn)
-	return version
+	version, goVersion, commitVersion, builtVersion, osArch, remoteApiVersion, err := GetVersion().Call(conn)
+
+	if err != nil {
+		fmt.Errorf("%s", err)
+		os.Exit(1)
+	}
+
+	return fmt.Sprintf("version: %s, golang: %s, commit: %s, built: %s, osArch: %s, remoteAPI: %d",
+		version, goVersion, commitVersion, builtVersion, osArch, remoteApiVersion)
 }
