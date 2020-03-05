2e147bd0169f9fb689f952109c9908060dfe3ea1
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-03-05 14:10:56 +0000

    Check dependencies first

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ func main() {
 	address := "unix:/run/user/1000/podman/io.podman"
 	conn := API.PodmanInit(address)
 
+	API.CheckDependencies()
 	API.GetPodmanVersion(conn)
 	API.PodmanPullImage(conn, "redis")
 }
